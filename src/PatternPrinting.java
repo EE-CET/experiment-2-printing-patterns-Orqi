07ae612fe1c7494ddb6cd1a178bb9e2c38ef4900
Amrita Reji <amritareji@Amritas-MBP.bbrouter>
2026-01-15 23:15:16 +0530

    add code

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // horizontal stars
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        // vertical stars
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+
+        sc.close();
+    }
 }
