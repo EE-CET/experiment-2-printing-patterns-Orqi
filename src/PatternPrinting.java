77014ba2b1f391a1bbcc0540be772490cff202e4
Amrita Reji <amritareji@Amritas-MBP.bbrouter>
2026-01-15 23:20:20 +0530

    fix code2

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,19 +1,14 @@
-import java.util.Scanner;
-
 public class PatternPrinting {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = 3;
 
         for (int i = 0; i < n; i++) {
-            System.out.print("* ");
+            System.out.print("*");
         }
         System.out.println();
 
         for (int i = 0; i < n; i++) {
             System.out.println("*");
         }
-
-        sc.close();
     }
 }
